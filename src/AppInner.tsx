2b4376348861bf518ea3abc77f2eb62e0ffd0d13
mnmn092631 <mnmn092631@gmail.com>
2025-01-18 21:38:58 +0900

    리덕스 연결하기

---
+++ src/AppInner.tsx
@@ -0,0 +1,67 @@
+import {NavigationContainer} from '@react-navigation/native';
+import Orders from './pages/Orders.tsx';
+import Delivery from './pages/Delivery.tsx';
+import Settings from './pages/Settings.tsx';
+import SignIn from './pages/SignIn.tsx';
+import SignUp from './pages/SignUp.tsx';
+import * as React from 'react';
+import {useSelector} from 'react-redux';
+import {RootState} from './store/reducer.ts';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+export type LoggedInParamList = {
+  Orders: undefined;
+  Settings: undefined;
+  Delivery: undefined;
+  Complete: {orderId: string};
+};
+
+export type RootStackParamList = {
+  SignIn: undefined;
+  SignUp: undefined;
+};
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppInner() {
+  const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+
+  return (
+    <NavigationContainer>
+      {isLoggedIn ? (
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Orders"
+            component={Orders}
+            options={{title: '오더 목록'}}
+          />
+          <Tab.Screen
+            name="Delivery"
+            component={Delivery}
+            options={{headerShown: false}}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{title: '내 정보'}}
+          />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{title: '로그인'}}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{title: '회원가입'}}
+          />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
+  );
+}
