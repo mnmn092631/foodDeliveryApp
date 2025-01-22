628b3b2a4d645c9ad00d0c44bb21c847b0e4e470
mnmn092631 <mnmn092631@gmail.com>
2025-01-22 22:48:04 +0900

    주문 목록 화면 만들기(FlatList), 주문 수락 및 거절 버튼 만들기

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,9 +1,11 @@
 import {combineReducers} from '@reduxjs/toolkit';
 
 import userSlice from '../slices/user.ts';
+import orderSlice from '../slices/order.ts';
 
 const rootReducer = combineReducers({
   user: userSlice.reducer,
+  order: orderSlice.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
