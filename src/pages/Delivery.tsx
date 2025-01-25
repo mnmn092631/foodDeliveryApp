b19f60c8c273452abc1f71fb3b82e439c6f8c5df
mnmn092631 <mnmn092631@gmail.com>
2025-01-25 19:43:17 +0900

    내 위치 가져오기(geolocation)

--- src/pages/Delivery.tsx
+++ src/pages/Delivery.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Ing from './Ing.tsx';
-import Complete from './Complete.tsx';
+import Ing from './Ing';
+import Complete from './Complete';
+import {LoggedInParamList} from '../AppInner';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<LoggedInParamList>();
 
 export default function Delivery() {
   return (
-    <Stack.Navigator initialRouteName="Ing">
-      <Stack.Screen name="Ing" component={Ing} options={{headerShown: false}} />
+    <Stack.Navigator initialRouteName="Delivery">
+      <Stack.Screen
+        name="Delivery"
+        component={Ing}
+        options={{headerShown: false}}
+      />
       <Stack.Screen
         name="Complete"
         component={Complete}
